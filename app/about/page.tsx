39d2788bc71e192fe9399988f19273373206d2ba
Karina Prado <karina.pradogutierrez@gmail.com>
2024-08-07 17:54:23 -0500

    feat: add info

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
 const About = () => {
   return (
     <div className="container mx-auto">
-      <h1>About Us</h1>
+      <h1 className="mb-4 mt-6 text-center text-3xl tracking-tight font-extrabold lg:text-5xl text-blue-600 dark:text-blue-700">
+        About Us
+      </h1>
+      <Image
+        src={"/about.jpg"}
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{ width: "100%", height: "400px" }}
+        alt={"About us banner"}
+      />
+      <p className="p-4">
+        Our company if formed by 4 people, we work together and want to change
+        the ecommerce industry by giving always a good service and quality.
+      </p>
     </div>
   );
 };
--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,9 +1,13 @@
+import { ShoppingCartOutlined } from "@ant-design/icons";
 import React from "react";
 
 const Cart = () => {
   return (
-    <div className="container mx-auto">
-      <h1>Cart</h1>
+    <div className="container mx-auto flex-row text-center">
+      <h1 className="mb-6 mt-6 text-center text-3xl tracking-tight font-extrabold lg:text-5xl text-blue-600 dark:text-blue-700">
+        CART
+      </h1>
+      <ShoppingCartOutlined style={{ fontSize: "20rem", color: "blue" }} />
     </div>
   );
 };
