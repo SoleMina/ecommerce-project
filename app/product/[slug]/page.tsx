a955d9be0b48248e8ff0bcd4c93de8bcb4888f7a
Karina Prado <karina.pradogutierrez@gmail.com>
2024-08-31 13:03:49 -0500

    feat: update component

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -3,13 +3,11 @@ import Counter from "@/app/components/Counter";
 import { ProductProps } from "@/types/Product";
 import Image from "next/image";
 import { useParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 
 const ProductDetail = () => {
   const [data, setData] = useState<ProductProps>();
   const { slug } = useParams();
-
-  console.log(slug, "slug");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,7 +19,7 @@ const ProductDetail = () => {
         if (response.ok) {
           const result = await response.json();
           console.log(result, "resultttt");
-          setData(result); // Set the fetched data to state
+          setData(result);
         } else {
           console.error("Failed to fetch data, status:", response.status);
         }
@@ -35,34 +33,37 @@ const ProductDetail = () => {
 
   return (
     <>
-      {data ? (
-        <div className="rounded overflow-hidden m-4 flex">
-          <div className="px-6 py-4">
-            <Image src={data.image} width={300} height={300} alt={data.title} />
-          </div>
-          <div className="px-6 py-4 pb-2">
-            <div className="pb-4">
-              <div className="font-bold text-3xl mb-2 pt-8">{data.title}</div>
-              <p className="text-gray-700 text-base">{data.description}</p>
+      <Suspense fallback={<div>Loading...</div>}>
+        {data && (
+          <div className="rounded overflow-hidden m-4 flex">
+            <div className="px-6 py-4">
+              <Image
+                src={data.image}
+                width={300}
+                height={300}
+                alt={data.title}
+              />
             </div>
-            <hr />
-            <Counter stock={data.stock} item={data} />
-            <div className="p-4">
-              <h4 className="mb-2">Categories:</h4>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                {data.category}
-              </span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                {data.price}
-              </span>
+            <div className="px-6 py-4 pb-2">
+              <div className="pb-4">
+                <div className="font-bold text-3xl mb-2 pt-8">{data.title}</div>
+                <p className="text-gray-700 text-base">{data.description}</p>
+              </div>
+              <hr />
+              <Counter stock={data.stock} item={data} />
+              <div className="p-4">
+                <h4 className="mb-2">Categories:</h4>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                  {data.category}
+                </span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                  {data.price}
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        <div>
-          <p>No hay productos con este id</p>
-        </div>
-      )}
+        )}
+      </Suspense>
     </>
   );
 };
